8821b44ce29c60e38542ef1c92f5014712d7a9e4
yassirelkhaili <elkhailiyassir@gmail.com>
2022-12-14 18:51:51 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [displayValue, setdisplayValue] = useState("display");
+  const clear = () => {
+    setdisplayValue("0");
+  };
+  const displayInput = (Input) => {
+    if (displayValue === "0" || displayValue === "display") {
+      setdisplayValue(Input);
+    } else {
+      setdisplayValue(displayValue + Input);
+    }
+    if (displayValue.length === 17) {
+        setdisplayValue("0");
+    }
+  };
+
+  const handleInput = () => {
+    setdisplayValue(eval(displayValue));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container rounded">
+      <div className="row">
+        <div
+          className="col-12 d-flex align-items-center justify-content-center"
+          id="display"
         >
-          Learn React
-        </a>
-      </header>
+          {displayValue}
+        </div>
+      </div>
+      <div className="row">
+        <button onClick={clear} className="col-6 bg-primary" id="clear">
+          AC
+        </button>
+        <button onClick={() => displayInput("/")} className="col-3" id="divide">
+          /
+        </button>
+        <button
+          onClick={() => displayInput("*")}
+          className="col-3"
+          id="multiply"
+        >
+          x
+        </button>
+      </div>
+      <div className="row">
+        <button onClick={() => displayInput("7")} className="col-3" id="seven">
+          7
+        </button>
+        <button onClick={() => displayInput("8")} className="col-3" id="eight">
+          8
+        </button>
+        <button onClick={() => displayInput("9")} className="col-3" id="nine">
+          9
+        </button>
+        <button
+          onClick={() => displayInput("-")}
+          className="col-3"
+          id="subtract"
+        >
+          -
+        </button>
+      </div>
+      <div className="row">
+        <button onClick={() => displayInput("4")} className="col-3" id="four">
+          4
+        </button>
+        <button onClick={() => displayInput("5")} className="col-3" id="five">
+          5
+        </button>
+        <button onClick={() => displayInput("6")} className="col-3" id="six">
+          6
+        </button>
+        <button onClick={() => displayInput("+")} className="col-3" id="add">
+          +
+        </button>
+      </div>
+      <div className="row">
+        <button onClick={() => displayInput("1")} className="col-3" id="one">
+          1
+        </button>
+        <button onClick={() => displayInput("2")} className="col-3" id="two">
+          2
+        </button>
+        <button onClick={() => displayInput("3")} className="col-3" id="three">
+          3
+        </button>
+        <button
+          onClick={() => displayInput(".")}
+          className="col-3"
+          id="decimal"
+        >
+          .
+        </button>
+      </div>
+      <div className="row">
+        <button onClick={() => displayInput(" 0")} className="col-6" id="zero">
+          0
+        </button>
+        <button onClick={handleInput} className="col-6 bg-warning" id="equals">
+          =
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
